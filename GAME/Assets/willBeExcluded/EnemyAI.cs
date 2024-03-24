547a20437f4a0d15716a9b42e110b4a6114613d0
Moser <guilhermemoserm@gmail.com>
2024-03-24 18:21:12 -0300

    Adicionado para ponto extra, o inimigo atirando

--- GAME/Assets/willBeExcluded/EnemyAI.cs
+++ GAME/Assets/willBeExcluded/EnemyAI.cs
@@ -5,14 +5,40 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    public NavMeshAgent enemy;
+    public Transform Player;
+    public GameObject enemyBullet;
+    public float enemySpeed;
+    [SerializeField] private float Timer = 5;
+    public float rangeToShoot;
+    public Transform spawnPoint;
+    bool isShooting;
 
-    public NavMeshAgent agent;
-
-    public Transform player; 
-   
     public void Update()
     {
-        agent.SetDestination(player.position);
+        enemy.SetDestination(Player.position);
+
+        float distance = Vector3.Distance(transform.position, Player.position);
+
+        if (distance <= rangeToShoot)
+        {
+            if (!isShooting)
+            {
+                InvokeRepeating("ShootAtPlayer", 0f, 0.5f);
+                isShooting = true;
+            }
+        }
+        else
+        {
+            CancelInvoke("ShootAtPlayer");
+        }
     }
 
+    void ShootAtPlayer()
+    {
+        GameObject bulletObj = Instantiate(enemyBullet, spawnPoint.position, Quaternion.identity);
+        Rigidbody bulletRig = bulletObj.GetComponent<Rigidbody>();
+        bulletRig.AddForce((Player.position - spawnPoint.position).normalized * enemySpeed, ForceMode.Impulse);
+        Destroy(bulletObj, 5f);
+    }
 }
